75894aa85bf14f505d01ba4c717789d0560cae46
EJLiu89 <edwardl8@vt.edu>
2020-04-04 23:44:19 -0400

    Implement dynamic add-as buttons

--- app/src/main/java/com/eightnineapps/coinly/activities/UserProfileActivity.kt
+++ app/src/main/java/com/eightnineapps/coinly/activities/UserProfileActivity.kt
@@ -2,6 +2,7 @@ package com.eightnineapps.coinly.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -13,7 +14,10 @@ import com.eightnineapps.coinly.classes.Notification
 import com.eightnineapps.coinly.enums.NotificationType.ADDING_AS_BIG
 import com.eightnineapps.coinly.enums.NotificationType.ADDING_AS_LITTLE
 import com.eightnineapps.coinly.classes.User
+import com.eightnineapps.coinly.enums.NotificationType
 import com.eightnineapps.coinly.fragments.HomeFragments.Companion.currentUserSnapshot
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 
 /**
  * Represents a single user of the app
@@ -47,11 +51,15 @@ class UserProfileActivity : AppCompatActivity() {
      */
     private fun setupButtons() {
         database.collection("users").document(ObservedUser.email!!).get().addOnCompleteListener{
-                task ->
-                if (task.isSuccessful) {
-                    val mostUpdatedObservedUser = task.result?.toObject(User::class.java)!!
-                    setUpAddAsBig(mostUpdatedObservedUser)
-                    setUpAddAsLittle(mostUpdatedObservedUser)
+                observedUserTask ->
+                database.collection("users").document(currentUser.email!!).get().addOnCompleteListener {
+                    currentUserTask ->
+                    if (observedUserTask.isSuccessful && currentUserTask.isSuccessful ) {
+                        val mostUpdatedObservedUser = observedUserTask.result?.toObject(User::class.java)!!
+                        val mostUpdatedCurrentUser = currentUserTask.result?.toObject(User::class.java)!!
+                        setUpAddAsBig(mostUpdatedObservedUser, mostUpdatedCurrentUser)
+                        setUpAddAsLittle(mostUpdatedObservedUser, mostUpdatedCurrentUser)
+                    }
                 }
         }
     }
@@ -59,7 +67,7 @@ class UserProfileActivity : AppCompatActivity() {
     /**
      * Sets up the "add as little" button and determines whether it should be enabled
      */
-    private fun setUpAddAsLittle(observedUser: User) {
+    private fun setUpAddAsLittle(observedUser: User, currentUser: User) {
         if (!alreadyRequested(false, observedUser) && !alreadyAdded(false, observedUser)) {
             addAsLittleButton.setOnClickListener {
                 sendAddNotification(false, observedUser)
@@ -73,14 +81,66 @@ class UserProfileActivity : AppCompatActivity() {
     /**
      * Sets up the "add as big" button and determines whether it should be enabled
      */
-    private fun setUpAddAsBig(observedUser: User) {
-        if (!alreadyRequested(true, observedUser) && !alreadyAdded(true, observedUser)) {
+    private fun setUpAddAsBig(observedUser: User, currentUser: User) {
+        val alreadyRequested = alreadyRequested(true, observedUser)
+        val alreadyAdded = alreadyAdded(true, observedUser)
+        val alreadyReceivedRequest = alreadyReceivedRequest(true, currentUser)
+        if (!alreadyRequested && !alreadyAdded && !alreadyReceivedRequest) {
+            Log.w("INFO", "THE WRONG BRANCH!")
             addAsBigButton.setOnClickListener {
                 sendAddNotification(true, observedUser)
+                addAsBigButton.text = getString(R.string.requested)
                 addAsBigButton.isEnabled = false
             }
-        } else {
+        } else if (alreadyRequested) { //YOU requested THEM
+            addAsBigButton.text = getString(R.string.requested)
             addAsBigButton.isEnabled = false
+        } else if (alreadyAdded) {
+            addAsBigButton.text = getString(R.string.added_as_big)
+            addAsBigButton.isEnabled = false
+        } else { //THEY requested YOU. alreadyReceivedRequest is true
+            Log.w("INFO", "THE CORRECT BRANCH!")
+            queryForPendingRequest(observedUser, ADDING_AS_LITTLE)
+        }
+    }
+
+    private fun queryForPendingRequest(observedUser: User, type: NotificationType) {
+        database.collection("users").document(currentUser.email!!).get().addOnCompleteListener {
+            updatedCurrentUserTask -> checkForPendingRequest(updatedCurrentUserTask, observedUser, type)
+        }
+    }
+
+    private fun checkForPendingRequest(updatedCurrentUserTask: Task<DocumentSnapshot>, observedUser: User, type: NotificationType) {
+        if (updatedCurrentUserTask.isSuccessful) {
+            val updatedCurrentUser = updatedCurrentUserTask.result!!.toObject(User::class.java)!!
+            val potentialNotification = updatedCurrentUser.notifications.find { it.type == type &&
+                    it.toAddUserEmail == updatedCurrentUser.email &&
+                    it.addingToUserEmail == observedUser.email } // should return just one
+            if (potentialNotification != null) {
+                addAsBigButton.text = getString(R.string.accept_request)
+                addAsBigButton.isEnabled = true
+                addAsBigButton.setOnClickListener {
+                    potentialNotification.execute()
+                    updatedCurrentUser.notifications.remove(potentialNotification)
+                    //updaate
+                    addAsBigButton.text = getString(R.string.added_as_big)
+                    addAsBigButton.isEnabled = false
+                }
+            } else { // This else branch should never execute because all other scenarios are covered by the if else blocks in the setUpAs method
+                addAsBigButton.isEnabled = false
+            }
+        }
+    }
+
+    private fun alreadyReceivedRequest(asBig: Boolean, mostUpdatedCurrentUser: User): Boolean {
+        return if (asBig) {
+            mostUpdatedCurrentUser.notifications.find { it.type == ADDING_AS_LITTLE &&
+                    it.toAddUserEmail == currentUser.email &&
+                    it.addingToUserEmail == ObservedUser.email } != null
+        } else {
+            mostUpdatedCurrentUser.notifications.find { it.type == ADDING_AS_BIG &&
+                    it.toAddUserEmail == currentUser.email &&
+                    it.addingToUserEmail == ObservedUser.email } != null
         }
     }
 
