f9b9571022f98e713b757feb7f68a3bb2b2d7932
EJLiu89 <edwardl8@vt.edu>
2020-04-13 23:51:16 -0400

    Create appeal & request coins activities

--- app/src/main/java/com/eightnineapps/coinly/activities/RequestCoinsActivity.kt
+++ app/src/main/java/com/eightnineapps/coinly/activities/RequestCoinsActivity.kt
@@ -1,27 +1,19 @@
 package com.eightnineapps.coinly.activities
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
 import com.eightnineapps.coinly.R
 
-class BigProfileActivity : AppCompatActivity() {
-
-    private lateinit var appealButton: Button
-    private lateinit var requestButton: Button
+class RequestCoinsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_big_profile)
-        appealButton = findViewById(R.id.appeal_button)
-        requestButton = findViewById(R.id.request_coins_button)
-        setUpButtons()
+        setContentView(R.layout.activity_request)
         addCoinlyActionBarTitle()
     }
 
@@ -32,18 +24,6 @@ class BigProfileActivity : AppCompatActivity() {
         super.finish()
         overridePendingTransition(0, 0)
     }
-
-    private fun setUpButtons() {
-        appealButton.setOnClickListener {
-            val intent = Intent(this, AppealActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-        }
-        requestButton.setOnClickListener {
-            val intent = Intent(this, RequestCoinsActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-        }
-    }
-
     /**
      * Sets the title of the action bar to the app name in the custom font through an image view
      */
@@ -56,4 +36,5 @@ class BigProfileActivity : AppCompatActivity() {
         actionBar.customView = v
         actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#ffffff")))
     }
+
 }
