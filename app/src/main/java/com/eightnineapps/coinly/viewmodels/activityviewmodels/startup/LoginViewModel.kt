022575cf44ab8fb826a8f2f4d93e706ce3eac4de
EJLiu89 <edwardl8@vt.edu>
2020-09-28 23:54:36 -0400

    Remove loginViewModel context dependency

--- app/src/main/java/com/eightnineapps/coinly/viewmodels/activityviewmodels/startup/LoginViewModel.kt
+++ app/src/main/java/com/eightnineapps/coinly/viewmodels/activityviewmodels/startup/LoginViewModel.kt
@@ -1,20 +1,10 @@
 package com.eightnineapps.coinly.viewmodels.activityviewmodels.startup
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.eightnineapps.coinly.classes.helpers.AuthHelper
-import com.eightnineapps.coinly.classes.objects.User
 import com.eightnineapps.coinly.models.Firestore
-import com.eightnineapps.coinly.views.activities.profiles.CreateProfileActivity
-import com.eightnineapps.coinly.views.activities.startup.HomeActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.GoogleAuthProvider
@@ -23,6 +13,7 @@ import javax.inject.Inject
 
 class LoginViewModel constructor(private val authHelper: AuthHelper) : ViewModel() {
 
+    @Suppress("UNCHECKED_CAST")
     class Factory @Inject constructor(private val authHelper: AuthHelper): ViewModelProvider.NewInstanceFactory() {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             return  LoginViewModel(authHelper) as T
@@ -30,63 +21,19 @@ class LoginViewModel constructor(private val authHelper: AuthHelper) : ViewModel
     }
 
     /**
-     * Goes to the main activity page if sign in was successful
+     * Queries the firestore to see if the current user has created a profile
      */
-    fun updateUI(context: Context) {
+    fun attemptToGetCurrentUSer(): Task<DocumentSnapshot>? {
         if (authHelper.getAuthUser() != null) {
-            Firestore.read(authHelper.getAuthUserEmail()).get().addOnCompleteListener { task -> handleQueryTask(task, context) }
+            return Firestore.read(authHelper.getAuthUserEmail()).get()
         }
+        return null
     }
 
     /**
-     * Populates home screen or re-directs to the profile creation activity depending on whether a document
-     * with the user's email was found
+     * Signs into firebase authentication through Google account
      */
-    private fun handleQueryTask(task: Task<DocumentSnapshot>, context: Context) {
-        if (task.isSuccessful) {
-            if (!task.result?.exists()!!) {
-                context.startActivity(Intent(context, CreateProfileActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
-            } else {
-                context.startActivity(Intent(context, HomeActivity::class.java)
-                    .putExtra("current_user", task.result?.toObject(User::class.java)!!)
-                    .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
-            }
-        } else {
-            Log.w("INFO", task.exception)
-        }
-    }
-
-    /**
-     * Handles the Google sign in event once it's completed
-     */
-    fun checkAndHandleActivityResultCode(requestCode: Int, data: Intent?, context: Context) {
-        if (requestCode == 1) handleGoogleAuth(GoogleSignIn.getSignedInAccountFromIntent(data), context)
-    }
-
-    /**
-     * Initiates Firebase authentication upon successful Google account authentication
-     */
-    private fun handleGoogleAuth(task: Task<GoogleSignInAccount>, context: Context) {
-        try {
-            initiateFirebaseAuthWithGoogle(task.getResult(ApiException::class.java)!!, context)
-        } catch (e: ApiException) {
-            updateUI(context)
-        }
-    }
-
-    /**
-     * Attempt sign-in to Firebase Authentication
-     */
-    private fun initiateFirebaseAuthWithGoogle(acct: GoogleSignInAccount, context: Context) {
-        authHelper.getAuth().signInWithCredential(GoogleAuthProvider.getCredential(acct.idToken, null))
-            .addOnCompleteListener(context as Activity) { task -> handleFirebaseAuth(task, context) }
-    }
-
-    /**
-     * Sends the user to the profile creating page upon successful Firebase authentication
-     */
-    private fun handleFirebaseAuth(task: Task<AuthResult>, context: Context) {
-        if (!task.isSuccessful) Toast.makeText(context, "Sign-in failed", Toast.LENGTH_SHORT).show()
-        updateUI(context)
+    fun attemptToSignIntoFirebase(acct: GoogleSignInAccount): Task<AuthResult> {
+        return authHelper.getAuth().signInWithCredential(GoogleAuthProvider.getCredential(acct.idToken, null))
     }
 }
--- app/src/main/java/com/eightnineapps/coinly/views/activities/startup/LoginActivity.kt
+++ app/src/main/java/com/eightnineapps/coinly/views/activities/startup/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.eightnineapps.coinly.views.activities.startup
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.eightnineapps.coinly.R
+import com.eightnineapps.coinly.classes.objects.User
 import com.eightnineapps.coinly.viewmodels.activityviewmodels.startup.LoginViewModel
+import com.eightnineapps.coinly.views.activities.profiles.CreateProfileActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_login.*
 import javax.inject.Inject
@@ -21,7 +29,6 @@ class LoginActivity : AppCompatActivity() {
 
     @Inject
     lateinit var loginViewModelFactory: LoginViewModel.Factory
-
     private lateinit var loginViewModel: LoginViewModel
 
     /**
@@ -30,7 +37,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginViewModel = ViewModelProvider(this, loginViewModelFactory).get(LoginViewModel::class.java)
-        loginViewModel.updateUI(this)
+        checkForReturningUser()
         setContentView(R.layout.activity_login)
         setupSignInButton()
     }
@@ -50,7 +57,47 @@ class LoginActivity : AppCompatActivity() {
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        loginViewModel.checkAndHandleActivityResultCode(requestCode, data, this)
+        if (requestCode == 1) {
+            try { signIntoFirebase(data) } catch (e: ApiException) { checkForReturningUser() }
+        }
+    }
+
+    /**
+     * Signs into firebase authentication and redirects the user
+     */
+    private fun signIntoFirebase(data: Intent?) {
+        loginViewModel.attemptToSignIntoFirebase(GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)!!)
+            .addOnCompleteListener(this as Activity) {
+                if (!it.isSuccessful) Toast.makeText(this, "Sign-in failed", Toast.LENGTH_SHORT).show()
+                checkForReturningUser()
+            }
+    }
+
+    /**
+     * Query the firestore to check if the current user is an existing one to determine
+     * which activity to launch
+     */
+    private fun checkForReturningUser() {
+        loginViewModel.attemptToGetCurrentUSer()?.addOnCompleteListener {
+            if (it.isSuccessful) {
+                handleUserQuery(it)
+            } else {
+                Log.e("INFO", it.exception.toString())
+            }
+        }
+    }
+
+    /**
+     * Redirects the user to appropriate activity depending on whether they've created a profile
+     */
+    private fun handleUserQuery(task: Task<DocumentSnapshot>) {
+        if (!task.result?.exists()!!) {
+            startActivity(Intent(this, CreateProfileActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+        } else {
+            startActivity(Intent(this, HomeActivity::class.java)
+                .putExtra("current_user", task.result?.toObject(User::class.java)!!)
+                .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+        }
     }
 
     /**
