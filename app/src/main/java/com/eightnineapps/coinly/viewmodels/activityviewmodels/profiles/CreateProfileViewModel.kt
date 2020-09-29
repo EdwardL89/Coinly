f9d1de90443e88d66df9f6c4cc63ad07c3aaa82c
EJLiu89 <edwardl8@vt.edu>
2020-09-29 09:52:33 -0400

    Remove context dependency from CreateProfileViewModel

--- app/src/main/java/com/eightnineapps/coinly/viewmodels/activityviewmodels/profiles/CreateProfileViewModel.kt
+++ app/src/main/java/com/eightnineapps/coinly/viewmodels/activityviewmodels/profiles/CreateProfileViewModel.kt
@@ -1,66 +1,36 @@
 package com.eightnineapps.coinly.viewmodels.activityviewmodels.profiles
 
-import android.content.Context
-import android.content.Intent
-import android.util.Log
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import androidx.lifecycle.ViewModel
-import com.bumptech.glide.Glide
 import com.eightnineapps.coinly.classes.helpers.AuthHelper
-import com.eightnineapps.coinly.classes.helpers.ImageUploadHelper
 import com.eightnineapps.coinly.classes.objects.User
 import com.eightnineapps.coinly.models.Firestore
 import com.eightnineapps.coinly.models.ImgStorage
-import com.eightnineapps.coinly.views.activities.startup.HomeActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.storage.UploadTask
 import java.io.ByteArrayOutputStream
 import kotlin.random.Random
 
 class CreateProfileViewModel : ViewModel() {
 
     val authHelper = AuthHelper()
-    val imgUploadHelper =
-        ImageUploadHelper()
     private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     private var userProfilePictureByteData = ByteArrayOutputStream().toByteArray()
+    private val STARTING_COIN_AMOUNT = 100
 
     /**
-     * Makes sure all fields are filled in before continuing
+     * Saves the byte data of the selected image for the user's profile picture
      */
-    fun verifyProfileCreationIsComplete(context: Context, realName: EditText, displayName: EditText, bio: EditText) {
-        if (noFieldsEmpty(realName, displayName, bio)) {
-            Toast.makeText(context, "Creating profile...", Toast.LENGTH_LONG).show()
-            uploadUserAndGoToHome(
-                User(
-                    realName.text.toString(),
-                    displayName.text.toString(),
-                    generateId(),
-                    authHelper.getAuthUserEmail(),
-                    bio.text.toString()
-                ), context)
-        } else {
-            Toast.makeText(context, "Info missing!", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    /**
-     * Loads selected image to an image view and prepares the image to be uploaded to Storage
-     */
-    fun handleGallerySelectionCompletion(requestCode: Int, resultCode: Int, data: Intent?, appContext: Context, context: Context, profilePictureImgView: ImageView) {
-        if (requestCode == 1 && resultCode == AppCompatActivity.RESULT_OK) {
-            Glide.with(appContext).load(data!!.data).into(profilePictureImgView)
-            userProfilePictureByteData = imgUploadHelper.prepareForFirebaseStorageUpload(data, context)
-        }
+    fun saveSelectedImageByteData(byteData: ByteArray) {
+        userProfilePictureByteData = byteData
     }
 
     /**
      * Determines whether or not all fields are empty
      */
-    private fun noFieldsEmpty(realName: EditText, displayName: EditText, bio: EditText): Boolean {
-        return userProfilePictureByteData.isNotEmpty() && realName.text.toString() != "" &&
-                displayName.text.toString() != "" && bio.text.toString() != ""
+    fun noFieldsEmpty(realName: String, displayName: String, bio: String): Boolean {
+        return userProfilePictureByteData.isNotEmpty() && realName != "" &&
+                displayName != "" && bio != ""
     }
 
     /**
@@ -71,26 +41,32 @@ class CreateProfileViewModel : ViewModel() {
     }
 
     /**
-     * Uploads the profile picture to Storage, downloads and saves the image's Uri for reuse, then
-     * uploads the user to the Firestore
+     * Saves a new user to the firestore
      */
-    private fun uploadUserAndGoToHome(newUser: User, context: Context) {
-        newUser.coins = 100
-        ImgStorage.insert(userProfilePictureByteData, "profile_pictures/${newUser.id}").addOnSuccessListener {
-            ImgStorage.read(newUser.id).addOnSuccessListener { uri -> newUser.profilePictureUri = uri.toString()
-                        Firestore.insert(newUser).addOnSuccessListener { goToHomePage(context, newUser)
-                            }.addOnFailureListener { Log.w("INFO", "Could not upload user to Firestore") }
-                    }.addOnFailureListener { Log.w("INFO", "Could not download from Storage") }
-            }.addOnFailureListener { Log.w("INFO", "Could not upload to Firebase storage") }
+    fun saveUser(newUser: User): Task<Void> {
+        return Firestore.insert(newUser)
     }
 
     /**
-     * Launches an intent to go to the home page activity
+     * Gets the uri of a profile picture of the given user id
      */
-    private fun goToHomePage(context: Context, user: User) {
-        val intent = Intent(context, HomeActivity::class.java)
-            .putExtra("current_user", user)
-            .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-        context.startActivity(intent)
+    fun getProfilePictureUri(newUserId: String) : Task<Uri> {
+        return ImgStorage.read(newUserId)
+    }
+
+    /**
+     * Saves a profile picture under the User's id in firebase storage
+     */
+    fun saveProfilePicture(newUserId: String): UploadTask {
+        return ImgStorage.insert(userProfilePictureByteData, "profile_pictures/$newUserId")
+    }
+
+    /**
+     * Instantiates and returns a new User object with the given attributes
+     */
+    fun createNewUser(realName: String, displayName: String, bio: String): User {
+        val newUser =  User(realName, displayName, generateId(), authHelper.getAuthUserEmail(), bio)
+        newUser.coins = STARTING_COIN_AMOUNT
+        return newUser
     }
 }
