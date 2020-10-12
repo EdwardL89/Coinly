a9483b9315ade4e8334095fce5596ae8d7fa0e69
EJLiu89 <edwardl8@vt.edu>
2020-10-11 20:11:15 -0400

    Refactor requestCoinsFragment to place firestore method in viewmodel

--- app/src/main/java/com/eightnineapps/coinly/viewmodels/activityviewmodels/profiles/BigProfileViewModel.kt
+++ app/src/main/java/com/eightnineapps/coinly/viewmodels/activityviewmodels/profiles/BigProfileViewModel.kt
@@ -2,13 +2,16 @@ package com.eightnineapps.coinly.viewmodels.activityviewmodels.profiles
 
 import androidx.lifecycle.ViewModel
 import com.eightnineapps.coinly.adapters.PrizesRecyclerViewAdapter
+import com.eightnineapps.coinly.classes.objects.Notification
 import com.eightnineapps.coinly.classes.objects.Prize
 import com.eightnineapps.coinly.classes.objects.User
+import com.eightnineapps.coinly.enums.NotificationType
 import com.eightnineapps.coinly.enums.PrizeTapLocation
 import com.eightnineapps.coinly.models.CurrentUser
 import com.eightnineapps.coinly.models.Firestore
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.QuerySnapshot
+import kotlin.random.Random
 
 class BigProfileViewModel: ViewModel() {
 
@@ -21,6 +24,7 @@ class BigProfileViewModel: ViewModel() {
     private var prizesClaimedQuery: Task<QuerySnapshot>? = null
     private var prizesSetAdapter: PrizesRecyclerViewAdapter? = null
     private var prizesClaimedAdapter: PrizesRecyclerViewAdapter? = null
+    private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
     /**
      * Determines whether or not the prizes set have been loaded
@@ -103,6 +107,14 @@ class BigProfileViewModel: ViewModel() {
     }
 
     /**
+     * Sends a request notification to the observed user
+     */
+    fun sendNotification(coinsRequesting: Int, reasonForRequest: String) {
+        val notification = constructRequestNotification(coinsRequesting, reasonForRequest)
+        Firestore.addNotification(observedUserInstance.email!!, notification)
+    }
+
+    /**
      * Removes the observed Big
      */
     fun removeBigAndSendBack() {
@@ -128,5 +140,30 @@ class BigProfileViewModel: ViewModel() {
         observedUserInstance.numOfLittles -= 1
         Firestore.update(observedUserInstance, "numOfLittles", observedUserInstance.numOfLittles.toString())
         Firestore.removeLittle(observedUserInstance.email!!, CurrentUser.getEmail()!!)
+    }
+
+    /**
+     * Constructs and returns a notification that holds information about a coin request
+     */
+    private fun constructRequestNotification(coinsRequesting: Int, reasonForRequest: String): Notification {
+        val notification = Notification()
+        notification.id = generateId()
+        val myDisplayName = CurrentUser.instance!!.displayName
+        notification.coins = coinsRequesting
+        notification.moreInformation = reasonForRequest
+        notification.type = NotificationType.REQUESTING_COINS
+        notification.message = "$myDisplayName requested $coinsRequesting coins"
+        notification.moreInformation = "$myDisplayName is requesting $coinsRequesting coins. \n\n Reason:\n$reasonForRequest"
+        notification.profilePictureUri = CurrentUser.instance!!.profilePictureUri
+        notification.addingToUserEmail = CurrentUser.instance!!.email!!
+        return notification
+    }
+
+
+    /**
+     * Generates a random 30 character, alphanumerical id for each user
+     */
+    private fun generateId(): String {
+        return (1..30).map { Random.nextInt(0, charPool.size) }.map(charPool::get).joinToString("")
     }
 }
