191dea2a4a83acceae778ddd5291403eea593d9b
EJLiu89 <edwardl8@vt.edu>
2020-11-05 16:04:45 -0500

    Implement floating action button to set new prizes

--- app/src/main/java/com/eightnineapps/coinly/views/fragments/profiles/little/LittleProfileFragment.kt
+++ app/src/main/java/com/eightnineapps/coinly/views/fragments/profiles/little/LittleProfileFragment.kt
@@ -42,7 +42,6 @@ import kotlinx.android.synthetic.main.fragment_little_profile.view_pager
 class LittleProfileFragment: Fragment() {
 
     private val littleProfileViewModel: LittleProfileViewModel by activityViewModels()
-    private val dialogCreator = PrizeDialogCreator()
     private var savedContext: Context? = null
 
     /**
@@ -117,10 +116,6 @@ class LittleProfileFragment: Fragment() {
             (context as Activity).finish()
             redrawAllLittlesPage()
         }
-//        set_prize_button.setOnClickListener {
-//            val openGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-//            startActivityForResult(openGallery, 1)
-//        }
     }
 
     /**
@@ -130,93 +125,4 @@ class LittleProfileFragment: Fragment() {
         HomeActivity.tabLayout.getTabAt(0)!!.select()
         HomeActivity.tabLayout.getTabAt(1)!!.select()
     }
-
-//    /**
-//     * Catches the result of the intent that opens the gallery to select a profile picture image
-//     */
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (data != null) {
-//            littleProfileViewModel.handleGallerySelectionCompletion(requestCode, resultCode, data, requireContext())
-//            openDialogue(data)
-//        }
-//    }
-//
-//    /**
-//     * Open a dialogue for the user to set the title and price of the new prize
-//     */
-//    @SuppressLint("InflateParams")
-//    private fun openDialogue(data: Intent?) {
-//        dialogCreator.setImageDataForDialog(data)
-//        val dialog = dialogCreator.createAlertDialog(null, requireContext(), R.layout.dialog_set_new_prize_layout)
-//        dialogCreator.showDialog(dialog)
-//        setUpDialogButtons(dialog)
-//    }
-//
-//    /**
-//     * Sets the actions the buttons in the set new prize dialog will do
-//     */
-//    private fun setUpDialogButtons(dialog: AlertDialog) {
-//        dialog.cancel_set_new_prize_button.setOnClickListener {
-//            dialog.cancel()
-//        }
-//        dialog.set_button.setOnClickListener {
-//            val title = dialog.prize_title.text.toString()
-//            val price = dialog.prize_price.text.toString()
-//            if (verifyPrizeBeforeSetting(title, price, dialog)) {
-//                no_prizes_set_image.visibility = View.INVISIBLE
-//                uploadSetPrize(title, Integer.parseInt(price))
-//                dialog.dismiss()
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Verifies that the info entered by the user for the new set prize is valid
-//     */
-//    private fun verifyPrizeBeforeSetting(title: String, price: String, dialog: AlertDialog): Boolean {
-//        if (hasReachedSetPrizeLimit()) {
-//            Toast.makeText(requireContext(), "You've reached the prizes set limit!", Toast.LENGTH_SHORT).show()
-//            dialog.dismiss()
-//        } else if (title == "" || price == "") {
-//            Toast.makeText(requireContext(), "Missing Fields", Toast.LENGTH_SHORT).show()
-//        } else if (price != "" && price[0] == '0') {
-//            Toast.makeText(requireContext(), "Price cannot be 0", Toast.LENGTH_SHORT).show()
-//        } else {
-//            return true
-//        }
-//        return false
-//    }
-//
-//    /**
-//     * Starts the process of saving the set prize to the firestore and storage
-//     */
-//    private fun uploadSetPrize(prizeTitle: String, prizePrice: Int) {
-//        Toast.makeText(requireContext(), "Uploading prize...", Toast.LENGTH_SHORT).show()
-//        val prizeId = littleProfileViewModel.generateId()
-//        val prizePath = littleProfileViewModel.generatePrizePath(prizeId)
-//        littleProfileViewModel.insertPrizeImageToStorage(prizePath).addOnCompleteListener {
-//            littleProfileViewModel.downloadImageUri(prizePath).addOnCompleteListener {
-//                uri -> saveSetPrizeToFirestore(Prize(prizeTitle, prizePrice, uri.result!!.toString(), prizeId))
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Saves the recently set prize to the firestore
-//     */
-//    private fun saveSetPrizeToFirestore(prize: Prize) {
-//        littleProfileViewModel.savePrizeInFireStore(prize).addOnCompleteListener {
-//            littleProfileViewModel.addSetPrizeToRecycler(prize)
-//            Toast.makeText(savedContext, "Prize Set!", Toast.LENGTH_SHORT).show()
-//        }
-//    }
-//
-//    /**
-//     * Determines whether the user has set a number of prizes equal to the limit of how many one
-//     * can set.
-//     */
-//    private fun hasReachedSetPrizeLimit(): Boolean {
-//        return setPrizesRecyclerView.adapter!!.itemCount == 10
-//    }
 }
