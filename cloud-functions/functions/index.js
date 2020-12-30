95e7897de1ea2b1e106d0609310525a2cb9f63fa
EJLiu89 <edwardl8@vt.edu>
2020-12-30 10:10:08 -0500

    Refactor FCM functions to identify coinly notification type

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -9,27 +9,19 @@ const db = admin.firestore();
 
 // Listen for any change on collection `notifications`
 exports.notificationListener = functions.firestore
-    .document('users/{userEmail}/notifications/{notificationId}').onWrite( async (change, context) => {
-
-        console.log("IN LISTENER FUNCTION!")
-
-        const usersRef = db.collection('users');
-        const snapshot = await usersRef.where('email', '==', context.params.userEmail).get();
-        snapshot.forEach(doc => {
-
-            console.log(doc.data().token);
-
+    .document('users/{userEmail}/notifications/{notificationId}').onCreate( async (snapshot, context) => {
+        const userDoc = await db.collection('users').where('email', '==', context.params.userEmail).get();
+        const notificationDoc = snapshot.data()
+        userDoc.forEach(doc => {
             const message = {
                 data: {
-                    title: 'Hello!',
+                    title: notificationDoc.type,
                     body: "You've got a new notification!"
                 },
                 token: doc.data().token
             };
-
             admin.messaging().send(message).then((response) => {
                 console.log('Successfully sent message:', response);
-                return true;
               })
               .catch((error) => {
                 console.log('Error sending message:', error);
